c355d536f1ba0b7c50e5b4b50b6b36b52d546057
Boaz Sze <boaz.sze@gmail.com>
2024-03-02 20:00:27 +0800

    Update core.py

--- utils/core.py
+++ utils/core.py
@@ -11,6 +11,7 @@ def get_env(key: str, default: Optional[str]) -> str:
 	return value
 
 def log(content: str, error: bool = False):
+	"""logs a message to the console"""
 	now = datetime.now()
 	print(f'[{now.isoformat()}] {content}', flush=True, file=sys.stderr if error else sys.stdout)
 
