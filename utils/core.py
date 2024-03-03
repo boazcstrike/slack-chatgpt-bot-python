2e91bcfcef2c7f27a12926ccad8fd0c098c81727
Boaz Sze <boaz.sze@gmail.com>
2024-03-03 22:58:00 +0800

    Add sanitize and remove args

--- utils/core.py
+++ utils/core.py
@@ -19,3 +19,33 @@ def validate_input(value: Optional[str]) -> bool:
 	"""checks if the input is valid (not None and not empty)"""
 	return value is not None and value.strip() != ''
 
+def sanitize_prompt(prompt):
+  allowed = string.ascii_letters + '"!()[];:-.,/_ ' + string.digits
+  prompt = prompt.replace('\n'," ").replace('\t', " ")
+  prompt = ''.join(filter(lambda x: x in allowed, prompt.encode('ASCII', "ignore").decode('ASCII')))
+
+  forbidden_strings = [" -o", " --out"]
+  for p in forbidden_strings:
+    if p in prompt:
+      return ""
+
+  return prompt.strip()
+
+def remove_args(args, args_to_remove):
+  # e.g. args_to_remove = ["n", "U"]
+  olist = args.split("-")
+
+  new_args = []
+  for o in olist:
+    ok = True
+    for p in args_to_remove:
+      if o.startswith(p):
+        ok = False
+    if ok:
+      n = o
+      if len(o) > 0 and not o.endswith(' '):
+        n = n + " "
+
+      new_args.append(n)
+
+  return new_args
